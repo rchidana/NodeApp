c07a57fc51d801321dc4524cc310dfe632a8094b
SudhinNandan <57177849+SudhinNandan@users.noreply.github.com>
2020-03-24 09:30:21 +0000

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello NodeApp!! \n");
+  response.end("Hellooooooo NodeApp!! \n");
 });
 
 // listen on localhost:8081
